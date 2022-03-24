703dfa2b15d946006b82e24570b0e53e5652b12c
Sergio <kurisergio777777@gmail.com>
2022-03-23 20:57:28 -0600

    commit tree 1ce3df87a4177b40ace8dc8d77f527667c567e70

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+console.log(`I'm ${age} years old. Happy birthday to me!`)
+debugger 
+
+};*/
+
+
+
+
+
+function writeCards(names, eventname) {
+    const finalmessages = [
+    ]
+    for (let i = 0; i < names.length; i++) {
+     let messages = `Thank you, ${names[i]}, for the wondeful ${eventname} gift!`
+    
+     finalmessages.push (messages)
+    
+  }
+  return finalmessages 
+}
+
+
+function countdown (integer) {
+let positiveint = integer
+console.log(positiveint)
+    while (positiveint>0) 
+        console.log(positiveint -=1)
+
+}
+
+countdown(20)
